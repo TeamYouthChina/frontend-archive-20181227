4a62c81d53422476932ea44d31f7a4f1bd0a5f86
Vv <yuwei@gwu.edu>
2018-11-25 14:25:47 -0800

    c# Please enter the commit message for your changes. Lines starting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
 
 import {App} from './App';
 
