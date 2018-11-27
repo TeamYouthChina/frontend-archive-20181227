71a69a5f0907554d5871f400566f89fdc8c26aeb
vv <yuwei@gwu.edu>
2018-11-26 20:17:49 -0500

    vv

--- src/ui/module/top-bar.js
+++ src/ui/module/top-bar.js
@@ -1,23 +1,61 @@
 import React from 'react';
+import { Menu, Icon ,Button} from 'antd';
+const SubMenu = Menu.SubMenu;
+const MenuItemGroup = Menu.ItemGroup;
 
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
+    this.state={current:'mail'};
   }
-
+  handleClick = (e) => {
+    console.log('click ', e);
+    this.setState({
+      current: e.key,
+    });
+  }
   render() {
     return (
-      <div
-        style={{
-          background: '#1569e0',
-          boxShadow: '0 0 4px 1px rgba(0,0,0,0.05), 2px 2px 6px 2px rgba(0,0,0,0.15)',
-          position: 'fixed',
-          height: '55px',
-          width: document.body.offsetWidth,
-          zIndex: 1
-        }}
-      >
-      </div>
+      <div>
+        <Menu
+          onClick={this.handleClick}
+         // selectedKeys={[this.state.current]}
+          mode="horizontal"
+          gutter={48}
+        >
+       
+          <Menu.Item key="mail">
+            <Icon type="mail" />YouthChina
+          </Menu.Item>
+          <Menu.Item >
+            <Button target="" type="dashed" icon="search">Search</Button>
+         
+            <Button type="primary" shape="circle" icon="search" />
+            
+          </Menu.Item>
+          <Menu.Item key="app" >
+            <Icon type="appstore" />为你精选
+          </Menu.Item>
+          <SubMenu title={<span className="submenu-title-wrapper">招聘</span>}>
+            <MenuItemGroup title="Item 1">
+              <Menu.Item key="setting:1">Option 1</Menu.Item>
+              <Menu.Item key="setting:2">Option 2</Menu.Item>
+            </MenuItemGroup>
+            <MenuItemGroup title="Item 2">
+              <Menu.Item key="setting:3">Option 3</Menu.Item>
+              <Menu.Item key="setting:4">Option 4</Menu.Item>
+            </MenuItemGroup>
+          </SubMenu>
+          <Menu.Item key="alipay">
+            <a href="" target="_blank" rel="noopener noreferrer">企业Live</a>
+          </Menu.Item>
+          <Menu.Item key="squre">
+            <a href="" target="_blank" rel="noopener noreferrer">青年社区</a>
+          </Menu.Item>
+        </Menu>
+        
+      </div
+       >
     );
   }
 }
