3ef1db3f206d90892cfb8d5e57a88a78eac6c3c3
gardness <gardness@live.com>
2018-11-29 17:01:35 -0500

    commit

--- src/ui/page/login.js
+++ src/ui/page/login.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
 import {TopBar} from '../module/top-bar';
 import {languageHelper} from "../../tool/language-helper";
+
+import {Layout, Menu, Breadcrumb} from 'antd';
+
+import {Form, Icon, Input, Button, Checkbox} from 'antd';
+import {TopProfile} from "../module/top-profile";
+
+
+const {Header, Content, Footer} = Layout;
 
 /* route: '/login' */
 
@@ -11,20 +18,85 @@ const i18n = [
     login: '登陆界面',
     username: '用户名',
     password: '密码',
-    button:'登陆'
+    button: '登陆'
   },
   {
     login: 'Login page',
     username: 'username',
     password: 'password',
-    button:'Login'
+    button: 'Login'
   }
 ];
+
+
+const FormItem = Form.Item;
+
+class NormalLoginForm extends React.Component {
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  render() {
+    const {getFieldDecorator} = this.props.form;
+    return (
+      <Form onSubmit={this.handleSubmit}
+            style={{
+              maxWidth: '300px'
+            }}
+      >
+        <FormItem>
+          {getFieldDecorator('userName', {
+            rules: [{required: true, message: 'Please input your username!'}],
+          })(
+            <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="Username"/>
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('password', {
+            rules: [{required: true, message: 'Please input your Password!'}],
+          })(
+            <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} type="password"
+                   placeholder="Password"/>
+          )}
+        </FormItem>
+        <FormItem>
+          {getFieldDecorator('remember', {
+            valuePropName: 'checked',
+            initialValue: true,
+          })(
+            <Checkbox>Remember me</Checkbox>
+          )}
+          <a
+            style={{
+              float: 'right'
+            }}
+            href="">Forgot password</a>
+          <Button type="primary" htmlType="submit"
+                  style={{
+                    width: '100%'
+                  }}
+          >
+            Log in
+          </Button>
+          Or <a href="">register now!</a>
+        </FormItem>
+      </Form>
+    );
+  }
+}
+
+const WrappedNormalLoginForm = Form.create()(NormalLoginForm);
+
 
 class Login extends React.Component {
   constructor(props) {
     super(props);
-    
+
     this.text = i18n[languageHelper(this.props.language)];
   }
 
@@ -34,7 +106,7 @@ class Login extends React.Component {
         style={{
           display: 'flex',
           flexDirection: 'column',
-          backgroundColor: '#f7b0f3'
+          backgroundColor: '#f7f7f7'
         }}
       >
         <TopBar language={this.props.language}/>
@@ -45,79 +117,9 @@ class Login extends React.Component {
             justifyContent: 'center'
           }}
         >
-          <div
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              width: 1224,
-              backgroundColor: '#ffffff'
-            }}
-          >
-            {this.text.login}
 
-            <div
-              style={{
-                backgroundColor: '#ffffff',
-                borderRadius: '3px',
-                boxShadow: '0 0 4px 1px rgba(0, 0, 0, 0.05), 2px 2px 2px 1px rgba(0, 0, 0, 0.05)',
-                display: 'flex',
-                flexDirection: 'column',
-                marginBottom: '24px'
-              }}
-            >
-              <div
-                style={{
-                  borderTopLeftRadius: '3px',
-                  borderTopRightRadius: '3px',
-                  padding: '16px',
-                }}
-              >
-                {this.text.username}
-                <p style={{
-                  color: 'rgba(0,0,0,0.8)',
-                  fontSize: '16px',
-                  fontWeight: 500,
-                  marginBottom: '4.8px',
-                  lineHeight: '1.33em'
-                }}>
-                  {<input type='text' value=''/>}
-                </p>
-              </div>
-              <div
-                style={{
-                  borderTopLeftRadius: '3px',
-                  borderTopRightRadius: '3px',
-                  padding: '16px',
-                }}
-              >
-                {this.text.password}
-                <p style={{
-                  color: 'rgba(0,0,0,0.8)',
-                  fontSize: '16px',
-                  fontWeight: 500,
-                  marginBottom: '4.8px',
-                  lineHeight: '1.33em'
-                }}>
-                  {<input type='text' value=''/>}
-                </p>
-              </div>
-              <button style={{
-                backgroundColor: 'transparent',
-                borderBottomLeftRadius: '3px',
-                borderBottomRightRadius: '3px',
-                borderWidth: '0px',
-                color: '#1569e0',
-                display: 'block',
-                fontSize: '13px',
-                fontWeight: '500',
-                lineHeight: '20px',
-                padding: '8px',
-                width: '100%'
-              }}>
-                {this.text.button}
-              </button>
-            </div>
-          </div>
+          <WrappedNormalLoginForm/>
+
         </div>
       </div>
     );
