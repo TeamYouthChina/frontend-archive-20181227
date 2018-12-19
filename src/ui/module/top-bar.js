53e229841161d0d9da34891f1e9a07bf9d0df82b
vv <yuwei@gwu.edu>
2018-12-18 21:35:16 -0500

    2#

--- src/ui/module/top-bar.js
+++ src/ui/module/top-bar.js
@@ -1,61 +1,162 @@
 import React from 'react';
-import { Menu, Icon ,Button} from 'antd';
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
-
+import { Navbar, NavbarBrand, NavbarNav, NavItem, NavLink, Mask, View,Dropdown, DropdownToggle, DropdownMenu,  DropdownItem, Fa,Col, MDBDropdownItem} from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
 class TopBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state={current:'mail'};
+    this.state = {
+      collapse: false,
+      isWideEnough: false
+    };
+    this.onClick = this.onClick.bind(this);
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      collapse: false,
+    };
   }
-  handleClick = (e) => {
-    console.log('click ', e);
+  onClick(){
     this.setState({
-      current: e.key,
+      collapse: !this.state.collapse
     });
   }
+  toggle() {
+    this.setState({ collapse: !this.state.collapse });
+  }
+  
   render() {
     return (
       <div>
-        <Menu
-          onClick={this.handleClick}
-         // selectedKeys={[this.state.current]}
-          mode="horizontal"
-          gutter={48}
-        >
-       
-          <Menu.Item key="mail">
-            <Icon type="mail" />YouthChina
-          </Menu.Item>
-          <Menu.Item >
-            <Button target="" type="dashed" icon="search">Search</Button>
-         
-            <Button type="primary" shape="circle" icon="search" />
+        <header>
+     
+          <Router>
+            <Navbar color="transparent" dark expand="md" fixed="top" scrolling>
+              <Col md="1">
+                <NavbarBrand href="/">
+                  <strong>YouthChina</strong>
+                </NavbarBrand>
+              </Col>
+             
+              <Col md="4">
+                <NavbarNav center>
+                  <NavItem to="#" >
+                    <NavLink to="#" onClick={this.toggle}>主  页</NavLink>
+                  </NavItem>
+                 
+                  <NavItem>
+                    <Dropdown>
+                      <DropdownToggle  nav>
+                        <div className="d-none d-md-inline">职  务</div>
+                      </DropdownToggle>
+                      <DropdownMenu  className="dropdown-dark"left style={{ background: '#bdbdbd' }}>
+                        <DropdownItem href="#!">实  习</DropdownItem>
+                        <MDBDropdownItem divider />
+                        <DropdownItem href="#!">校园招聘</DropdownItem>
+                        <MDBDropdownItem divider />
+                        <DropdownItem href="#!">社会招聘</DropdownItem>
+                       
+                      </DropdownMenu>
+                    </Dropdown>
+                  </NavItem>
+                  <NavItem>
+                    <Dropdown>
+                      <DropdownToggle  nav>
+                        <div className="d-none d-md-inline">探  索</div>
+                      </DropdownToggle>
+                      <DropdownMenu  className="dropdown-dark"left style={{ background: '#bdbdbd' }}>
+                        <DropdownItem href="#!">文  章</DropdownItem>
+                        <MDBDropdownItem divider />
+                        <DropdownItem href="#!">视  频</DropdownItem>
+                        <MDBDropdownItem divider />
+                        <DropdownItem href="#!">人  脉</DropdownItem>
+                        <MDBDropdownItem divider />
+                        <DropdownItem href="#!">直  播（空中宣讲会）</DropdownItem>
+
+                      </DropdownMenu>
+                    </Dropdown>
+                  </NavItem>
+                
+                </NavbarNav>
+              </Col>
+              
+              <NavbarNav>
+                <Dropdown >
+                  <DropdownToggle  nav>
+                    <input
+                      className="form-control"
+                      type="text"
+                      placeholder="搜索"
+                      aria-label="Search"
+                    />
+                  </DropdownToggle>
+                  <DropdownMenu  className="dropdown-dark"  style={{ marginLeft: '1em', verticalAlign: 'middle' }} >
+                    <DropdownItem href="#!">企  业</DropdownItem>
+                    <MDBDropdownItem divider />
+                    <DropdownItem href="#!">公  司</DropdownItem>
+                    <MDBDropdownItem divider />
+                    <DropdownItem href="#!">人  脉</DropdownItem>
+                    <MDBDropdownItem divider />
+                    <DropdownItem href="#!">文  章</DropdownItem>
+                    <MDBDropdownItem divider />
+                    <DropdownItem href="#!">视  频</DropdownItem>
+
+                  </DropdownMenu>
+                </Dropdown>
+                
+                  
+              </NavbarNav>
+              
+             
+              <NavbarNav right>
+                <NavItem>
+                  <NavLink className="waves-effect waves-light" to="#!"><Fa icon="twitter" /></NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="waves-effect waves-light" to="#!"><Fa icon="google-plus" /></NavLink>
+                </NavItem>
+                <NavItem>
+                  <Dropdown>
+                    <DropdownToggle nav>
+                      <Fa icon="user" />
+                    </DropdownToggle>
+                    <DropdownMenu color="dark" basic right>
+                      <DropdownItem href="#!">个人主页</DropdownItem>
+                      <DropdownItem href="#!">我的消息</DropdownItem>
+                      <DropdownItem href="#!">我的申请</DropdownItem>
+                      <DropdownItem href="#!">我的好友</DropdownItem>
+                      <DropdownItem href="#!">我的关注</DropdownItem>
+                      <DropdownItem href="#!">帮助</DropdownItem>
+                      <DropdownItem href="#!">设置</DropdownItem>
+                      <DropdownItem href="#!">退出</DropdownItem>
+                    </DropdownMenu>
+                  </Dropdown>
+                </NavItem>
+              </NavbarNav>
+             
+            </Navbar>
             
-          </Menu.Item>
-          <Menu.Item key="app" >
-            <Icon type="appstore" />为你精选
-          </Menu.Item>
-          <SubMenu title={<span className="submenu-title-wrapper">招聘</span>}>
-            <MenuItemGroup title="Item 1">
-              <Menu.Item key="setting:1">Option 1</Menu.Item>
-              <Menu.Item key="setting:2">Option 2</Menu.Item>
-            </MenuItemGroup>
-            <MenuItemGroup title="Item 2">
-              <Menu.Item key="setting:3">Option 3</Menu.Item>
-              <Menu.Item key="setting:4">Option 4</Menu.Item>
-            </MenuItemGroup>
-          </SubMenu>
-          <Menu.Item key="alipay">
-            <a href="" target="_blank" rel="noopener noreferrer">企业Live</a>
-          </Menu.Item>
-          <Menu.Item key="squre">
-            <a href="" target="_blank" rel="noopener noreferrer">青年社区</a>
-          </Menu.Item>
-        </Menu>
+          </Router>
+       
         
-      </div
-       >
+  
+
+            
+          
+    
+          <View>
+            
+            <img className="d-block w-100" src="https://ws1.sinaimg.cn/large/94365dd2ly1fyarzle8e4j218g0hn77z.jpg" />
+            <Mask overlay="black-light" style={{flexDirection: 'column', height: '100vh'}} className="flex-center  text-white text-center">
+              <h2>YouthChina</h2>
+              <h5>欢迎，这是一句话</h5>
+              <br/>
+              <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
+            </Mask>
+          </View>
+
+         
+        </header>
+        
+      </div>
     );
   }
 }
