c4bdc0a3e83e67c40262949255cdcd1d5d250295
Zhicheng Zhang <zzc-tongji@users.noreply.github.com>
2018-12-23 10:41:13 -0500

    merge

--- src/ui/page/home.js
+++ src/ui/page/home.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarNav,
+  NavItem,
+  NavLink,
+  NavbarToggler,
+  Collapse,
+  FormInline,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Col,
+  Row
+} from 'mdbreact';
 
 import {languageHelper} from '../../tool/language-helper';
-import {JobListHome} from '../module/job-list-home';
 import {TopBar} from '../module/top-bar';
 import {FooterPage} from '../module/foot';
+import {JobCardSquare} from '../module/job-card-square';
+
 /* route: '/' */
 
 const i18n = [
@@ -37,7 +56,7 @@ const css = {
   }
 };
 
-class Home extends React.Component {
+export class Home extends React.Component {
   constructor(props) {
     super(props);
     /*
@@ -59,6 +78,34 @@ class Home extends React.Component {
         }}
       >
         <TopBar language={this.props.language}/>
+        <Navbar expand="md">
+          <NavbarNav center>
+            <NavItem>
+              <NavLink
+                onClick={
+                  () => {
+                    this.setState({selectedTab: 1});
+                  }
+                }
+                to={`${this.props.location.pathname}#`}
+              >
+                {i18n[languageHelper()].hot}
+              </NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink
+                onClick={
+                  () => {
+                    this.setState({selectedTab: 2});
+                  }
+                }
+                to={`${this.props.location.pathname}#`}
+              >
+                {i18n[languageHelper()].new}
+              </NavLink>
+            </NavItem>
+          </NavbarNav>
+        </Navbar>
         <div
           style={{
             display: 'flex',
@@ -73,83 +120,45 @@ class Home extends React.Component {
               width: 1224,
             }}
           >
-            <div
-              style={{
-                display: 'flex',
-              }}
-            >
-              <button
-                style={css.button}
-                onClick={
-                  () => {
-                    this.setState(
-                      () => {
-                        return {
-                          selectedTab: 1
-                        };
-                      }
+            {
+              (
+                () => {
+                  let tag;
+                  if (this.state.selectedTab === 2) {
+                    return null;
+                  } else { // this.state.selectedTab === 1
+                    return (
+                      <div>
+                        <JobListHome
+                          name={this.text.it}
+                          search={{
+                            industry: 'IT',
+                            tagList: [
+                              `${tag}`
+                            ],
+                            page: 1,
+                            size: 4,
+                            skipAuth: true
+                          }}
+                        />
+                        <JobListHome
+                          name={this.text.finance}
+                          search={{
+                            industry: 'finance',
+                            tagList: [
+                              `${tag}`
+                            ],
+                            page: 1,
+                            size: 4,
+                            skipAuth: true
+                          }}
+                        />
+                      </div>
                     );
                   }
                 }
-              >
-                {this.text.hot}
-              </button>
-              <button
-                style={css.button}
-                onClick={
-                  () => {
-                    this.setState(
-                      () => {
-                        return {
-                          selectedTab: 2
-                        };
-                      }
-                    );
-                  }
-                }
-              >
-                {this.text.new}
-              </button>
-            </div>
-            {(() => {
-              let tag;
-              switch (this.state.selectedTab) {
-                case 2:
-                  tag = 'new';
-                  break;
-                default:
-                  tag = 'hot';
-                  break;
-              }
-              return (
-                <div>
-                  <JobListHome
-                    name={this.text.it}
-                    search={{
-                      industry: 'IT',
-                      tagList: [
-                        `${tag}`
-                      ],
-                      page: 1,
-                      size: 4,
-                      skipAuth: true
-                    }}
-                  />
-                  <JobListHome
-                    name={this.text.finance}
-                    search={{
-                      industry: 'finance',
-                      tagList: [
-                        `${tag}`
-                      ],
-                      page: 1,
-                      size: 4,
-                      skipAuth: true
-                    }}
-                  />
-                </div>
-              );
-            })()}
+              )()
+            }
           </div>
         </div>
         <FooterPage/>
@@ -158,4 +167,81 @@ class Home extends React.Component {
   }
 }
 
-export {Home};
+class JobListHome extends React.Component {
+
+  constructor(props) {
+    super(props);
+    /*
+    * */
+    this.state = {};
+  }
+
+  componentWillMount() {
+    let mockData =
+      {
+        searchResult: ['1', '2', '3', '4'],
+        status: {
+          code: 2000
+        }
+      };
+    this.setState(() => {
+      return {backend: mockData};
+    });
+  }
+
+  render() {
+    return (this.state.backend && this.state.backend.status && this.state.backend.status.code === 2000) ? (
+      <div
+        style={{
+          padding: '24px 16px'
+        }}
+      >
+        <div
+          style={{
+            marginBottom: '16px'
+          }}
+        >
+          <span style={{
+            fontSize: '24px'
+          }}>
+            {this.props.name}
+          </span>
+        </div>
+        <Row>
+          {
+            this.state.backend.searchResult.map(
+              (item, index) =>
+                <Col
+                  key={index}
+                >
+                  <JobCardSquare
+                    id={item}
+                  />
+                </Col>
+            )
+          }
+        </Row>
+        <div>
+          <div>
+            <a
+              href="https://www.google.com/"
+              style={{
+                marginTop: '8px',
+                display: 'inline-block',
+                fontWeight: '500',
+                color: '#1569e0'
+              }}
+            >
+              See More →
+            </a>
+          </div>
+        </div>
+      </div>
+    ) : null;
+  }
+}
+
+JobListHome.propTypes = {
+  name: PropTypes.string.isRequired,
+  search: PropTypes.object.isRequired
+};
